52df90bd78cabcb070953ef6cd10de78f51ae33e
Alexey Kholodov <alexeyhol@yandex.ru>
2020-10-28 14:29:25 +0700

    Added layouts

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,9 @@
-<h1>About blog</h1>
+@extends('layouts.app')
+
+@section ('title', 'About blog')
+
+@section('header', 'About blog')
+
+@section('content')
 <p>Experiments with Hexlet</p>
+@endsection
